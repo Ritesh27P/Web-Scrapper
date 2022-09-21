06124d7dab3a1ccf21233f090b416898349e4313
ritesh-blog27 <riteshminecraft27@gmail.com>
2022-09-21 19:58:37 +0530

    First Commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from flask import Flask, render_template, request, url_for
+import time
 
 
 driver_path = "D:\SetupFile\chromedriver_win32\chromedriver.exe"
@@ -11,38 +12,40 @@ driver.get('https://news.google.com/')
 
 news = []
 
-data = driver.find_element(By.CLASS_NAME, 'ipQwMb ekueJc RD0gLb').text
+data = driver.find_element(By.CLASS_NAME, 'DY5T1d').text
 news.append(data)
+print(data)
 
-
+driver.get('https://news.google.com/')
 input_search = driver.find_element(By.XPATH, "/html/body/div[4]/header/div[2]/div[2]/div/form/div[1]/div/div/div/div/div[1]/input[2]")
 input_search.click();
 input_search.send_keys('HDFC bank')
 input_search.send_keys(Keys.ENTER);
 
-content = driver.find_element(By.CLASS_NAME, 'ipQwMb ekueJc RD0gLb').text
+content = driver.find_element(By.CLASS_NAME, 'xrnccd').text
+print(content)
 news.append(content)
 
-input_search = driver.find_element(By.XPATH, '/html/body/div[4]/header/div[2]/div[2]/div/form/div[1]/div/div/div/div/div[1]/input[2]');
-input_search.clear()
-input_search.send_keys('WIPRO')
-input_search.send_keys(Keys.ENTER)
+# input_search = driver.find_element(By.XPATH, '/html/body/div[4]/header/div[2]/div[2]/div/form/div[1]/div/div/div/div/div[1]/input[2]');
+# input_search.clear()
+# input_search.send_keys('WIPRO')
+# input_search.send_keys(Keys.ENTER)
 
-content1 = driver.find_element(By.CLASS_NAME, 'ipQwMb ekueJc RD0gLb').text
-news.append(content1)
+# content1 = driver.find_element(By.CLASS_NAME, 'ipQwMb ekueJc RD0gLb').text
+# news.append(content1)
 
-
-# time.sleep(10);
+print(news)
+time.sleep(5);
 driver.quit()
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    global news
-    return render_template('index.html', data=news)
+# @app.route('/')
+# def home():
+#     global news
+#     return render_template('index.html', data=news)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)

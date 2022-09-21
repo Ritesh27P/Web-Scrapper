da9de1e844a73d9feb56f0babc4f1875594bccdb
ritesh-blog27 <riteshminecraft27@gmail.com>
2022-09-21 20:02:24 +0530

    Minor Changes

--- main.py
+++ main.py
@@ -26,26 +26,26 @@ content = driver.find_element(By.CLASS_NAME, 'xrnccd').text
 print(content)
 news.append(content)
 
-# input_search = driver.find_element(By.XPATH, '/html/body/div[4]/header/div[2]/div[2]/div/form/div[1]/div/div/div/div/div[1]/input[2]');
-# input_search.clear()
-# input_search.send_keys('WIPRO')
-# input_search.send_keys(Keys.ENTER)
+input_search = driver.find_element(By.XPATH, '/html/body/div[4]/header/div[2]/div[2]/div/form/div[1]/div/div/div/div/div[1]/input[2]');
+input_search.clear()
+input_search.send_keys('WIPRO')
+input_search.send_keys(Keys.ENTER)
 
-# content1 = driver.find_element(By.CLASS_NAME, 'ipQwMb ekueJc RD0gLb').text
-# news.append(content1)
+content1 = driver.find_element(By.CLASS_NAME, 'ipQwMb ekueJc RD0gLb').text
+news.append(content1)
 
 print(news)
 time.sleep(5);
 driver.quit()
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 
-# @app.route('/')
-# def home():
-#     global news
-#     return render_template('index.html', data=news)
+@app.route('/')
+def home():
+    global news
+    return render_template('index.html', data=news)
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
